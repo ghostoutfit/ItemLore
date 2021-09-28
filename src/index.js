d82ff08d99261aa8e9210b595a9dfc6c023beaea
ghostoutfit <josephlkremer@gmail.com>
2021-09-28 14:06:57 -0400

    Added import Display

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-//import Display from './Display'
+import Display from './ReactDisplay';
 //import  {Query} from './Query';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Display />
   </React.StrictMode>,
   document.getElementById("root")
 );
